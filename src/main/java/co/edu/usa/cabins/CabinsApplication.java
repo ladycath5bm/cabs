76682b97548ca71fd5dbc9c8d55469fed5340c90
ladycath5bm <leidycath.bm@gmail.com>
2022-08-07 12:30:32 -0500

    feat: 🎸 first commit

---
+++ src/main/java/co/edu/usa/cabins/CabinsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usa.cabins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/usa/cabins/CabinsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usa.cabins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
